5d1050434b12edfc471e6831820a42c12d16cee9
AKINOLA EPAPHRAS OLUWATIMILEHIN <88168149+akinolaepaphras@users.noreply.github.com>
2022-06-01 00:44:23 +0100

    corrected a bug in the js

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ console.log(numberBeingConverted);
     let meter2 = numberBeingConverted / 3.28084
     document.getElementById("meter2").textContent = meter2.toFixed(3)
 
-    let gallon1 = numberBeingConverted / 0.264
+    let gallon1 = numberBeingConverted * 0.264
     document.getElementById("gallon1").textContent = gallon1.toFixed(3)
 
-    let liter2   = numberBeingConverted * 0.264
+    let liter2 = numberBeingConverted / 0.264
     document.getElementById("liter2").textContent = liter2.toFixed(3)
 
-    let pound1 = numberBeingConverted / 2.205 
+    let pound1 = numberBeingConverted * 2.205 
     document.getElementById("pound1").textContent = pound1.toFixed(3)
 
-    let kilo2 = numberBeingConverted * 2.205
+    let kilo2 = numberBeingConverted / 2.205
     document.getElementById("kilo2").textContent = kilo2.toFixed(3)
     
     
